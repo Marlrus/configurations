6ed8c85bc1a321a8706dd6a7236a1ea1fe415e00
Marlrus <36910211+Marlrus@users.noreply.github.com>
2023-01-02 20:01:41 -0500

    Packages for ubuntu 22.04 nvim clipboard problems

--- ubuntu-22-04-packages.sh
+++ ubuntu-22-04-packages.sh
@@ -2,5 +2,10 @@
 sudo apt install libfuse2
 python3 -m pip install --user --upgrade pynvim
 sudo apt install wl-clipboard
+sudo apt install xclip
 # System Customization Packages
 sudo apt install gnome-shell-extension-manager
+
+# Share Screen Workaround
+# sudo vi /etc/gdm3/custom.conf
+# WaylandEnable=false
