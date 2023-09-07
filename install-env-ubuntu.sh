c308e719201ad0d9b8f6bfe2d526eb59567cb1b6
Marlrus <36910211+Marlrus@users.noreply.github.com>
2023-09-07 12:42:20 -0500

    Refined kitty install script and moved it into and independent file

--- install-env-ubuntu.sh
+++ install-env-ubuntu.sh
@@ -55,13 +55,8 @@ sudo install lazygit /usr/local/bin
 rm lazygit*
 echo "===================== LAZYGIT INSTALLED ====================="
 
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-sudo ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten /usr/bin/
-cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
-sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/kitty 50
+chmod +x ./install-update-kitty.sh
+./install-update-kitty.sh
 echo "===================== KITTY INSTALLED ====================="
 
 mkdir ${CONFIG_URL}/kitty
