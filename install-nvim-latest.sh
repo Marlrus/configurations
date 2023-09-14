63f3da455052b8762d49c5da1fc498844a0b22bc
Marlrus <36910211+Marlrus@users.noreply.github.com>
2023-09-13 20:17:50 -0500

    Added yellow print fn to modules, updated err exit to print red, using yellow print on nvim script

--- install-nvim-latest.sh
+++ install-nvim-latest.sh
@@ -6,24 +6,24 @@ source ./modules.sh
 FN_PRINT_GREEN_BANNER "NVIM INSTALLATION/UPDATE BEGINNING"
 
 if [ -x "$(command -v python3 -V)" ]; then
-  echo "Python 3 found."
+  FN_PRINT_YELLOW "Python 3 found."
 else
-  echo "Python 3 not found, please install"
+  FN_PRINT_YELLOW "Python 3 not found, please install"
   exit 1
 fi
 
 if [ -x "$(command -v pip -V)" ]; then
-  echo "Pip found."
+  FN_PRINT_YELLOW "Pip found."
 else
-  echo "Pip not found, installing pip."
+  FN_PRINT_YELLOW "Pip not found, installing pip."
   sudo apt install python3-pip -y
   FN_PRINT_GREEN_BANNER "PIP INSTALLED"
 fi
 
 if [ $(pip list | rg "pynvim" | wc -l) = 1 ]; then
-  echo "Pynvim found."
+  FN_PRINT_YELLOW "Pynvim found."
 else
-  echo "Pynvim not found, installing pynvim."
+  FN_PRINT_YELLOW "Pynvim not found, installing pynvim."
   python3 -m pip install --user --upgrade pynvim
   FN_PRINT_GREEN_BANNER "INSTALLED"
 fi
@@ -35,40 +35,48 @@ if [ -f ${HOME_PATH}/nvim.appimage ]; then
 fi
 
 wget -q --show-progress https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -P ${HOME_PATH}/ &&
-echo "Nvim latest dowloaded to home directory." ||
+FN_PRINT_YELLOW "Nvim latest dowloaded to home directory." ||
 FN_ERR_EXIT "Error dowloading NVIM"
 
 wget -q --show-progress https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage.sha256sum -P ${HOME_PATH}/ &&
-echo "Downloading Checksum" ||
+FN_PRINT_YELLOW "Downloading Checksum" ||
 FN_ERR_EXIT "Error dowloading NVIM Checksum"
 
 if [ -f ${HOME_PATH}/nvim.appimage ] && [ -f ${HOME_PATH}/nvim.appimage.sha256sum ]; then
-  echo "Verifying checksum"
+  FN_PRINT_YELLOW "Verifying checksum"
   # In case checksum changes
   CHECKSUM=$(awk '{print $1}' ${HOME_PATH}/nvim.appimage.sha256sum)
   if [[ $(sha256sum ${HOME_PATH}/nvim.appimage | awk '{print $1}') != $CHECKSUM ]]; then
-    FN_ERR_EXIT "Checksum not matching"
+    cp ${HOME_PATH}/nvim.appimage-prev ${HOME_PATH}/nvim.appimage
+    FN_ERR_EXIT "Checksum not matching, reverting nvim to previous appimage"
   else
-    echo "Checksum matches."
+    FN_PRINT_YELLOW "Checksum matches."
   fi
   rm ${HOME_PATH}/nvim.appimage.sha256sum
   chmod +x ${HOME_PATH}/nvim.appimage
-  echo "Execution permissions added to nvim"
+  FN_PRINT_YELLOW "Execution permissions added to nvim"
   if ! command grep -qc  '/nvim.appimage' ${HOME_PATH}/.bashrc; then
-    echo "Appending nvim alias to bash .bashrc"
+    FN_PRINT_YELLOW "Appending nvim alias to bash .bashrc"
     printf '\nalias nvim="/home/${USER}/nvim.appimage"\n' >> ${HOME_PATH}/.bashrc
     source ${HOME_PATH}/.bashrc
   else
-    echo "alias for nvim appimage found."
+    FN_PRINT_YELLOW "alias for nvim appimage found."
   fi
 else
-  FN_ERR_EXIT "NVIM app image not found" 
+  cp ${HOME_PATH}/nvim.appimage-prev ${HOME_PATH}/nvim.appimage
+  FN_ERR_EXIT "Checksum not matching, reverting nvim to previous appimage"
 fi
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 FN_PRINT_GREEN_BANNER "VIM PLUG INSTALLED/UPDATED"
 
-FN_PRINT_GREEN_BANNER "NVIM INSTALLED"
-FN_PRINT_GREEN_BANNER "CHECKING NVIM VERSION"
+FN_PRINT_GREEN_BANNER "NVIM INSTALLED/UPDATED"
+
+if [ -f ${HOME_PATH}/nvim.appimage-prev ]; then
+  FN_PRINT_GREEN_BANNER "PREVIOUS NVIM VERSION"
+  ${HOME_PATH}/nvim.appimage-prev -v
+fi
+
+FN_PRINT_GREEN_BANNER "NEW NVIM VERSION"
 ${HOME_PATH}/nvim.appimage -v
--- modules.sh
+++ modules.sh
@@ -1,5 +1,7 @@
 HOME_PATH="/home/${USER}"
 GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+RED="\033[0;31m" 
 NO_COLOR="\033[0m"
 GITHUB_EMAIL="36910211+Marlrus@users.noreply.github.com"
 GITHUB_NAME="Marlrus"
@@ -8,7 +10,11 @@ function FN_PRINT_GREEN_BANNER(){
   printf "\n${GREEN}[==================== $1 ====================]${NO_COLOR}\n\n"
 }
 
+function FN_PRINT_YELLOW(){
+  printf "${YELLOW}$1${NO_COLOR}\n"
+}
+
 function FN_ERR_EXIT(){
-  echo "Error: $1"
+  printf "${RED}Error:${NO_COLOR} $1\n"
   exit 1
 }
