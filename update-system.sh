a8078b56d3f955a3e00790dd63b8acf863c580c8
Marlrus <36910211+Marlrus@users.noreply.github.com>
2024-09-20 11:48:55 -0500

    Added line to update docker compose plugin

--- update-system.sh
+++ update-system.sh
@@ -8,6 +8,8 @@ FN_PRINT_GREEN_BANNER "SYSTEM UPDATE BEGINNING"
 sudo apt update && sudo apt -y upgrade ||
 FN_ERR_EXIT "Error updating with apt"
 
+sudo cp -r /usr/libexec/docker/cli-plugins/docker-compose /usr/lib/docker/cli-plugins/docker-compose
+
 FN_PRINT_GREEN_BANNER "PACKAGE MANAGER PACKAGES UPDATED"
 
 ./update-dev-packages.sh ||
