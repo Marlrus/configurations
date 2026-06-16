87cea80bd9429327af50f3bc14deb716ae1e5374
Marlrus <36910211+Marlrus@users.noreply.github.com>
2026-06-15 22:24:18 -0500

    Removed some optional programs

--- install-optional-programs.sh
+++ install-optional-programs.sh
@@ -5,9 +5,6 @@ source ./modules.sh
 
 FN_PRINT_GREEN_BANNER "OPTIONAL PROGRAM INSTALLATION BEGINNING"
 
-sudo snap install keep-presence
-FN_PRINT_GREEN_BANNER "KEEP PRESENCE INSTALLED"
-
 sudo add-apt-repository ppa:obsproject/obs-studio -y
 sudo apt install obs-studio -y
 FN_PRINT_GREEN_BANNER "OBS INSTALLED"
@@ -15,10 +12,10 @@ FN_PRINT_GREEN_BANNER "OBS INSTALLED"
 sudo snap install vlc
 FN_PRINT_GREEN_BANNER "VLC INSTALLED"
 
-sudo snap install steam
+# sudo snap install steam
 sudo snap install postman
 
-sudo apt install darktable
-sudo apt install rawtherapee
+# sudo apt install darktable
+# sudo apt install rawtherapee
 
 sudo apt install pandoc
